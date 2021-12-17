86e59b1fdef1da00bcbd7d19ed03eb6bab69c900
Mikheeva Alexandra <im89118350092@gmail.com>
2021-12-18 00:06:42 +0300

    project init

---
+++ Ozon_2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ozon_2.0
+//
+//  Created by Михеева Александра Германовна on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+}
+
